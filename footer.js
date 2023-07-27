da6d73af6ec9410292150ccd876c9173fdb9e41a
D_Binara <dbkaveesha@gmail.com>
2023-07-27 11:50:45 +0530

    put comment and testing js files

---
+++ footer.js
@@ -0,0 +1,13 @@
+
+document.write(
+<footer className="py-3 my-4">
+    <ul className="nav justify-content-center border-bottom pb-3 mb-3  navbar-dark bg-dark">
+        <li className="nav-item"><a href="Home.html" className="nav-link px-2 text-muted">Home</a></li>
+        <li className="nav-item"><a href="Link.html" className="nav-link px-2 text-muted">Store</a></li>
+        <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Other Details</a></li>
+        <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
+        <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">About</a></li>
+    </ul>
+    <nav className="text-center text-muted">© 2022 Company, Inc</nav>
+</footer>
+)
