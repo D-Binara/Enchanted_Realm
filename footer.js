7ba7624ae3c97b6cb0d269e4e2d0340e29381f04
D_Binara <dbkaveesha@gmail.com>
2023-09-30 00:12:52 +0530

    update

--- footer.js
+++ footer.js
@@ -6,13 +6,13 @@ class Footer extends HTMLElement {
             integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
             <footer class="py-3 my-4">
                 <ul class="nav justify-content-center border-bottom pb-3 mb-3  navbar-dark bg-dark">
-                    <li className="nav-item"><a href="Home.html" className="nav-link px-2 text-muted">Home</a></li>
-                    <li className="nav-item"><a href="Link.html" className="nav-link px-2 text-muted">Store</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">Other Details</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">FAQs</a></li>
-                    <li className="nav-item"><a href="#" className="nav-link px-2 text-muted">About</a></li>
+                    <li class="nav-item"><a href="Home.html" class="nav-link px-2 text-muted">Home</a></li>
+                    <li class="nav-item"><a href="Link.html" class="nav-link px-2 text-muted">Store</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Other Details</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+                    <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
                 </ul>
-                <nav className="text-center text-muted">© 2022 Company, Inc</nav>
+                <nav class="text-center text-muted">© 2022 Company, Inc</nav>
             </footer>
         `;
     }
